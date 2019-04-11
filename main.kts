f8a0616d6ffd710bb341f8ad7915ae0ce358185e
Elisa <elisat15@uw.edu>
2019-04-11 13:31:08 -0700

    pass all test

--- main.kts
+++ main.kts
@@ -3,14 +3,106 @@
 println("UW Homework: Simple Kotlin")
 
 // write a "whenFn" that takes an arg of type "Any" and returns a String
+fun whenFn(x: Any): String {
+    when (x) {
+        "Hello" -> return "world"
+        is String -> return "Say what?"
+        0 -> return "zero"
+        1 -> return "one"
+        in 2..10 -> return "low number"
+        else -> return "I don't understand"
+    }
+}
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
+fun add(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
+fun sub(num1: Int, num2: Int): Int {
+    return num1 - num2
+}
+
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
+fun mathOp(num1: Int,
+           num2: Int,
+           function: (funNum1: Int, funNum2: Int) -> Int): Int {
+    return function(num1, num2)
+}
 
 // write a class "Person" with first name, last name and age
+class Person (var firstName: String, var lastName: String, var age: Int) {
+    
+    public var debugString: String = "[Person firstName:${this.firstName} lastName:${this.lastName} age:${this.age}]"
+        get() {
+            return "[Person firstName:${this.firstName} lastName:${this.lastName} age:${this.age}]" 
+        }
+    
+    fun equals(other: Person): Boolean {
+        if (other.firstName != this.firstName)
+            return false
+        else if (other.lastName != this.lastName)
+            return false
+        else if (other.age != this.age)
+            return false
+
+        return true
+    }
+
+    override
+    fun hashCode(): Int {
+        var hashCode = 0
+
+        hashCode = 31 * hashCode + this.firstName.hashCode()
+        hashCode = 31 * hashCode + this.lastName.hashCode()
+        hashCode = 31 * hashCode + this.age
+
+        return hashCode
+    }
+
+}
 
 // write a class "Money"
+class Money constructor(var userAmount: Int, var userCurrency: String) {
+    public var amount: Int = userAmount
+        set(value) {
+            if (value > 0)
+                userAmount = value
+        }
+    public var currency: String = userCurrency
+        set(value) {
+            if (value == "USD" || value == "CAN" || value == "EUR" || value == "GBP") 
+                userCurrency = value
+        }
+
+    public fun convert(convertCurrency: String): Money {
+
+        var convertedAmount = amount;
+        
+        // converts to USD
+        if (currency == "EUR" || currency == "CAN")
+            convertedAmount = (convertedAmount / 15).times(10)
+        else if (currency == "GBP")
+            convertedAmount = (convertedAmount / 5).times(10)
+
+        if (convertCurrency == "EUR") 
+            return Money((convertedAmount / 10).times(15) , convertCurrency)
+        else if (convertCurrency == "CAN") 
+            return Money((convertedAmount / 10).times(15), convertCurrency)
+        else if (convertCurrency == "GBP")
+            return Money((convertedAmount / 10).times(5), convertCurrency)
+        else 
+            return Money(convertedAmount, convertCurrency)
+    }
+
+    operator fun plus(other: Money): Money {
+
+        return Money((other.convert(currency).amount + amount), currency)
+
+    }
+
+}
 
 // ============ DO NOT EDIT BELOW THIS LINE =============
 
@@ -84,6 +176,7 @@ val convert_tests = listOf(
     Pair(fiveGBP, fifteenEUR)
 )
 for ( (from,to) in convert_tests) {
+    // println("${from.amount} ${from.currency} to ${to.amount} ${to.currency} results: ${from.convert(to.currency).amount}")
     print(if (from.convert(to.currency).amount == to.amount) "." else "!")
 }
 val moneyadd_tests = listOf(
